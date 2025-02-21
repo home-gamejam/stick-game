060777b291447d1691069dbfeb4e65f0a74da646
Brian Ingles <github@emeraldwalk.com>
2025-02-20 22:22:44 -0600

    multiplayer camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody3D
 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = deg_to_rad(75)
-@onready var _player_camera := %PlayerCamera as PlayerCamera
 @onready var _animation_tree = %AnimationTree as AnimationTree
 @onready var _model = %Model as Node3D
 
+const PLAYER_CAMERA = preload("res://scenes/player_camera.tscn")
 const ACCELERATION = 20.0
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -15,10 +15,17 @@ const ROTATION_SPEED = TAU * 2
 var is_running = false
 var last_direction := Vector3.BACK
 var speed = SPEED
+var _player_camera: PlayerCamera
 
 func _enter_tree():
 	# this assumes that name was set to the pid
 	set_multiplayer_authority(int(str(name)))
+
+func _ready():
+	if is_multiplayer_authority():
+		_player_camera = PLAYER_CAMERA.instantiate()
+		add_child(_player_camera)
+
 
 func _physics_process(delta: float) -> void:
 	#if Engine.is_editor_hint():
@@ -74,7 +81,7 @@ func _physics_process(delta: float) -> void:
 	_animation_tree.set("parameters/Movement/blend_position", blend_position)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion && _player_camera:
 		_player_camera.rotation.x -= event.relative.y * mouse_sensitivity
 		_player_camera.rotation.x = clampf(_player_camera.rotation.x, -tilt_limit, tilt_limit)
 		_player_camera.rotation.y += -event.relative.x * mouse_sensitivity
