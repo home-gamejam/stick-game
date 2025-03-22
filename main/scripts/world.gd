26a41e3f5b6b47d2ca346090b0c5e0b984f3e340
Brian Ingles <github@emeraldwalk.com>
2025-03-21 21:41:10 -0500

    Moved lobby load instead of preload to avoid invalid path in web build

--- main/scripts/world.gd
+++ main/scripts/world.gd
@@ -2,15 +2,14 @@ extends Node3D
 
 
 const PLAYER_SCENE = preload("res://main/player.tscn")
-const LOBBY_SCENE = preload("res://client/lobby.tscn")
-const LOBBY_WEBRTC_SCENE = preload("res://signal_ws/signal_ws_lobby.tscn")
 
 # Set this to determine client / server or WebRTC lobby
 var IS_WEB_RTC = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var lobby = LOBBY_WEBRTC_SCENE.instantiate() if IS_WEB_RTC else LOBBY_SCENE.instantiate()
+	var lobby_path = "res://signal_ws/signal_ws_lobby.tscn" if IS_WEB_RTC else "res://client/lobby.tscn"
+	var lobby = load(lobby_path).instantiate()
 	add_child(lobby)
 
 	lobby.player_added.connect(_on_player_added)
