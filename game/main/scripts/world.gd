b9799fabb3d61755a71de9060c500a1a764ac043
Brian Ingles <github@emeraldwalk.com>
2025-03-31 23:14:37 -0500

    Signal client moved to submodule

--- game/main/scripts/world.gd
+++ game/main/scripts/world.gd
@@ -4,13 +4,14 @@ extends Node3D
 const PLAYER_SCENE = preload("res://main/player.tscn")
 
 # Set this to determine client / server or WebRTC lobby
-var IS_WEB_RTC = true
+# var IS_WEB_RTC = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var lobby_path = "res://signal_ws/signal_ws_lobby.tscn" if IS_WEB_RTC else "res://client/lobby.tscn"
-	var lobby = load(lobby_path).instantiate()
-	add_child(lobby)
+	# var lobby_path = "res://signal_ws/signal_ws_lobby.tscn" if IS_WEB_RTC else "res://client/lobby.tscn"
+	# var lobby = load(lobby_path).instantiate()
+	# add_child(lobby)
+	var lobby = %SignalLobby
 
 	lobby.player_added.connect(_on_player_added)
 
--- game/signal_ws/README.md
+++
@@ -1,6 +0,0 @@
-# Signaling Server - WebSockets
-
-- lobby host button clicked
-- client: connect_to_server() called, peer created, lobby and peer id both set to 0, ws connects to server
-- server: poll() picks up on available client connection, creates peer, sends CONNECTED msg back to client
-- client: poll() picks up CONNECTED msg, sets peer id from the message, sends HOST or JOIN msg back to server for the peer based on whether lobby_id is known or not, emits connected event which causes mesh to be initialized for the current peer.
