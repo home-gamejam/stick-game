158aff1748b93f4d932daac966dd67168d688f67
Brian Ingles <github@emeraldwalk.com>
2025-04-08 22:36:59 -0500

    Cleanup

--- game/main/scripts/game.gd
+++ game/main/scripts/game.gd
@@ -9,6 +9,7 @@ const PLAYER_SCENE = preload("res://main/player.tscn")
 
 var game_started = false
 
+
 func _ready() -> void:
 	var lobby = %SignalLobby
 
@@ -16,20 +17,24 @@ func _ready() -> void:
 	lobby.lobby_sealed.connect(_on_lobby_sealed)
 
 
+# When players are added, add them to the waiting room. If game has started,
+# move them to the game world.
 func _on_player_added(pid: int):
-	print("[game] _on_player_added: ", pid)
-	print("[game] Adding player to waiting list: ", pid)
 	waiting_room.add_player(pid)
 
 	if game_started:
 		_move_players_to_game();
 
 
+# Move players to game world when the lobby is sealed and mark the game as
+# started.
 func _on_lobby_sealed(_lobby_id: int):
 	_move_players_to_game()
 	game_started = true
 
 
+# Move players from the waiting room to the game world and toggle visibility of
+# UI elements so that game world is visible.
 func _move_players_to_game():
 	signal_lobby.hide()
 	waiting_room.hide()
