5e0a09d334451ef1429150f38007b39c3d0143c9
Brian Ingles <github@emeraldwalk.com>
2025-04-25 18:14:50 -0500

    Fixed diagonal animation blend

--- game/state/states/move_state.gd
+++ game/state/states/move_state.gd
@@ -47,8 +47,8 @@ func physics_process(delta: float) -> State:
 	# blend_position is 0, 1, 2 for idle, walk, run respectively. Multiplying
 	# walk or run by the input_dir magnitude should hit the values exactly when
 	# value is 1 and a blend when it is < 1
-	var blend_position = (2 if is_running else 1) * input_dir.length()
-
+	var blend_position = (2 if is_running else 1) * input_dir.normalized().length()
+	print("blend_position: ", blend_position)
 	character.set_animation_blend_position(blend_position)
 
 	return null
