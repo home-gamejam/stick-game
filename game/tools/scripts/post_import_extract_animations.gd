cc54e26aee0c805a533596586ff85e4231acdaee
Brian Ingles <github@emeraldwalk.com>
2025-05-11 22:10:18 -0500

    Remove AnimationPlayer from post import glb

--- game/tools/scripts/post_import_extract_animations.gd
+++ game/tools/scripts/post_import_extract_animations.gd
@@ -5,6 +5,11 @@ func _post_import(scene):
 	for node in scene.get_children():
 		if node is AnimationPlayer:
 			var anim_lib = node.get_animation_library("")
-			
+			print("[Post import] animation player: ", node.name)
 			ResourceSaver.save(anim_lib, "res://resources/" + scene.name + "_animations.tres")
+
+			# Remove the AnimationPlayer from the scene
+			node.get_parent().remove_child(node)
+			node.queue_free()
+
 	return scene
