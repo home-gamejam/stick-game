056ba6064d920633f75aaa639c603b90d093ae2f
Brian Ingles <github@emeraldwalk.com>
2025-05-26 19:28:49 -0500

    Moved camera logic to camera

--- game/player/scripts/player_camera.gd
+++ game/player/scripts/player_camera.gd
@@ -2,4 +2,20 @@ extends Node3D
 
 class_name PlayerCamera
 
+@export var model: CharacterModel:
+	set(model_):
+		model = model_
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75)
+
 @onready var camera := %Camera as Camera3D
+
+func _physics_process(_delta: float) -> void:
+	position = model.position
+	model.adjusted_basis = camera.global_basis
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clampf(rotation.x, -tilt_limit, tilt_limit)
+		rotation.y += -event.relative.x * mouse_sensitivity
--- game/player/scripts/player_controller.gd
+++ game/player/scripts/player_controller.gd
@@ -1,9 +1,6 @@
 #@tool
 extends CharacterController
 
-@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
-@export var tilt_limit = deg_to_rad(75)
-
 const PLAYER_CAMERA = preload("res://player/player_camera.tscn")
 
 var _player_camera: PlayerCamera
@@ -18,22 +15,8 @@ func _ready():
 		return
 
 	_player_camera = PLAYER_CAMERA.instantiate()
+	_player_camera.model = character_model
 	add_child(_player_camera)
-
-	character_model.adjusted_basis = _player_camera.camera.global_basis
-
-func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
-
-	if _player_camera:
-		_player_camera.position = character_model.position
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion && _player_camera:
-		_player_camera.rotation.x -= event.relative.y * mouse_sensitivity
-		_player_camera.rotation.x = clampf(_player_camera.rotation.x, -tilt_limit, tilt_limit)
-		_player_camera.rotation.y += -event.relative.x * mouse_sensitivity
-		character_model.adjusted_basis = _player_camera.camera.global_basis
 
 func get_input() -> InputData:
 	return InputData.get_current()
