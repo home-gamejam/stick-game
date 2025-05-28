c78872791978444d70cde7f2151ed1db6cb7abdb
Brian Ingles <github@emeraldwalk.com>
2025-05-27 22:24:49 -0500

    Constrain camera

--- TODO.md
+++ TODO.md
@@ -2,7 +2,8 @@
 
 - [x] Handle input events
 - [x] Fix multiplayer sync
-- [ ] Get rid of stickman prefix on animations
+- [x] Get rid of stickman prefix on animations
 - [ ] Save animations
 - [ ] Keypad
 - [ ] Is there a way to rotate the character body instead of the rig node
+- [x] Limit camera ot not go through floor
--- game/player/scripts/player_camera.gd
+++ game/player/scripts/player_camera.gd
@@ -4,11 +4,12 @@ class_name PlayerCamera
 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = 0.5 # deg_to_rad(75)
+var tilt_limit_min = 0.0
 
 @onready var camera := %Camera as Camera3D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation.x -= event.relative.y * mouse_sensitivity
-		rotation.x = clampf(rotation.x, -tilt_limit, tilt_limit)
+		rotation.x = clampf(rotation.x, tilt_limit_min, tilt_limit)
 		rotation.y += -event.relative.x * mouse_sensitivity
