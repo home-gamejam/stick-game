0b3ffaa32afe26c12eafdba467e670058d6d79fd
Brian Ingles <github@emeraldwalk.com>
2025-06-13 22:03:45 -0500

    Explicit maps

--- art/blender/scripts/rigify-to-godot2.py
+++ art/blender/scripts/rigify-to-godot2.py
@@ -3,20 +3,10 @@ import bpy
 import re
 from bpy.types import Armature, BoneCollection, Pose
 
-REPARENT_BONE_MAP = {
-    "LeftBreast": "UpperChest",
-    "RightBreast": "UpperChest",
-    "LeftShoulder": "UpperChest",
-    "RightShoulder": "UpperChest",
-    "LeftUpperArm": "LeftShoulder",
-    "RightUpperArm": "RightShoulder",
-    "LeftPelvis": "Hips",
-    "RightPelvis": "Hips",
-    "LeftUpperLeg": "Hips",
-    "RightUpperLeg": "Hips",
-}
-
 RENAME_BONE_MAP = {
+    "root": "Root",
+
+    # Spine
     "DEF-spine": "Hips",
     "DEF-spine.001": "Spine",
     "DEF-spine.002": "Chest",
@@ -24,6 +14,11 @@ RENAME_BONE_MAP = {
     "DEF-spine.004": "Neck",
     "DEF-spine.005": "Neck2",
     "DEF-spine.006": "Head",
+
+    # Breasts
+    "DEF-breast.L": "LeftBreast",
+    "DEF-breast.R": "RightBreast",
+
     # Left arm
     "DEF-shoulder.L": "LeftShoulder",
     "DEF-upper_arm.L": "LeftUpperArm",
@@ -31,6 +26,7 @@ RENAME_BONE_MAP = {
     "DEF-forearm.L": "LeftLowerArm",
     "DEF-forearm.L.001": "LeftLowerArm2",
     "DEF-hand.L": "LeftHand",
+
     # Right arm
     "DEF-shoulder.R": "RightShoulder",
     "DEF-upper_arm.R": "RightUpperArm",
@@ -38,12 +34,11 @@ RENAME_BONE_MAP = {
     "DEF-forearm.R": "RightLowerArm",
     "DEF-forearm.R.001": "RightLowerArm2",
     "DEF-hand.R": "RightHand",
-    # Breasts
-    "DEF-breast.L": "LeftBreast",
-    "DEF-breast.R": "RightBreast",
+
     # Pelvis
     "DEF-pelvis.L": "LeftPelvis",
     "DEF-pelvis.R": "RightPelvis",
+
     # Left leg
     "DEF-thigh.L": "LeftUpperLeg",
     "DEF-thigh.L.001": "LeftUpperLeg2",
@@ -51,6 +46,7 @@ RENAME_BONE_MAP = {
     "DEF-shin.L.001": "LeftLowerLeg2",
     "DEF-foot.L": "LeftFoot",
     "DEF-toe.L": "LeftToes",
+
     # Right leg
     "DEF-thigh.R": "RightUpperLeg",
     "DEF-thigh.R.001": "RightUpperLeg2",
@@ -60,15 +56,64 @@ RENAME_BONE_MAP = {
     "DEF-toe.R": "RightToes",
 }
 
+PARENT_BONE_MAP = {
+    # Spine chain
+    "Hips": "Root",
+    "Spine": "Hips",
+    "Chest": "Spine",
+    "UpperChest": "Chest",
+    "Neck": "UpperChest",
+    "Neck2": "Neck",
+    "Head": "Neck2",
+
+    # Breasts
+    "LeftBreast": "UpperChest",
+    "RightBreast": "UpperChest",
+
+    # Arms
+    "LeftShoulder": "UpperChest",
+    "LeftUpperArm": "LeftShoulder",
+    "LeftUpperArm2": "LeftUpperArm",
+    "LeftLowerArm": "LeftUpperArm2",
+    "LeftLowerArm2": "LeftLowerArm",
+    "LeftHand": "LeftLowerArm2",
+
+    "RightShoulder": "UpperChest",
+    "RightUpperArm": "RightShoulder",
+    "RightUpperArm2": "RightUpperArm",
+    "RightLowerArm": "RightUpperArm2",
+    "RightLowerArm2": "RightLowerArm",
+    "RightHand": "RightLowerArm2",
+
+    # Pelvis
+    "LeftPelvis": "Hips",
+    "RightPelvis": "Hips",
+
+    # Legs
+    "LeftUpperLeg": "Hips",
+    "LeftUpperLeg2": "LeftUpperLeg",
+    "LeftLowerLeg": "LeftUpperLeg2",
+    "LeftLowerLeg2": "LeftLowerLeg",
+    "LeftFoot": "LeftLowerLeg2",
+    "LeftToes": "LeftFoot",
+
+    "RightUpperLeg": "Hips",
+    "RightUpperLeg2": "RightUpperLeg",
+    "RightLowerLeg": "RightUpperLeg2",
+    "RightLowerLeg2": "RightLowerLeg",
+    "RightFoot": "RightLowerLeg2",
+    "RightToes": "RightFoot",
+}
+
 def main():
     print("Rigify to Godot")
 
     armature = bpy.context.active_object
 
-    if not armature or armature.type != "ARMATURE" or not isinstance(armature.data, Armature):
-        print("No armature selected or active object is not an armature.")
-        return
-
+    assert armature is not None, "No active object found."
+    assert armature.type == "ARMATURE", "Active object is not an armature."
+    assert isinstance(armature.data, Armature), "Active object data is not an Armature."
+    assert bpy.context.view_layer is not None, "No active view layer found."
     assert bpy.context.collection is not None, "No active collection found."
 
     bpy.ops.object.mode_set(mode="OBJECT")
@@ -78,21 +123,23 @@ def main():
     target_armature = bpy.data.objects.new(f"{armature.name}.target", target_armature_data)
     bpy.context.collection.objects.link(target_armature)
 
-    # Extract names while in OBJECT mode since collection bones are not
-    # accessible in EDIT mode
-    def_names = [b.name for b in armature.data.collections["DEF"].bones]
+    bpy.ops.object.mode_set(mode="EDIT")
+    source_bones = [b for b in armature.data.edit_bones if b.name in RENAME_BONE_MAP.keys()]
+    bpy.ops.object.mode_set(mode="OBJECT")
 
-    # Select the object
+    # Deselect the source armature
     bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = None
+
+    # Select the target armature
     target_armature.select_set(True)
+    bpy.context.view_layer.objects.active = target_armature
 
     bpy.ops.object.mode_set(mode="EDIT")
-
-    def_bones = [b for b in armature.data.edit_bones if b.name in def_names]
-
     tgt_edit_bones = target_armature_data.edit_bones
 
-    for def_bone in def_bones:
+    # Copy bones to target armature
+    for def_bone in source_bones:
         new_name = RENAME_BONE_MAP[def_bone.name] if def_bone.name in RENAME_BONE_MAP else def_bone.name
         print("TGT bone: ", new_name)
         tgt_bone = tgt_edit_bones.new(new_name)
@@ -100,9 +147,8 @@ def main():
         tgt_bone.tail = def_bone.tail
         tgt_bone.roll = def_bone.roll
 
-    # Not all of the original DEF- bones are properly parented in Rigify, so fix
-    # the TGT versions.
-    for bone_name, parent_name in REPARENT_BONE_MAP.items():
+    # Set target bone parents
+    for bone_name, parent_name in PARENT_BONE_MAP.items():
         if bone_name in tgt_edit_bones and parent_name in tgt_edit_bones:
             print(f"Set parent: {bone_name} -> {parent_name}")
             tgt_edit_bones[bone_name].parent = tgt_edit_bones[parent_name]
