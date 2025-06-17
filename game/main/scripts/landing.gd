44268cad2e73b8e557c4077d04171d5237411cd4
Brian Ingles <github@emeraldwalk.com>
2025-06-17 08:52:13 -0500

    Update web button

--- game/main/scripts/landing.gd
+++ game/main/scripts/landing.gd
@@ -7,8 +7,15 @@ func _ready() -> void:
 	%Play.pressed.connect(_on_play_pressed)
 	%Multiplayer.pressed.connect(_on_multiplayer_pressed)
 
+	%UpdateGame.visible = OS.get_name() == "Web"
+	if %UpdateGame.visible:
+		%UpdateGame.pressed.connect(_on_update_game)
+
 func _on_play_pressed() -> void:
 	play_pressed.emit()
 
 func _on_multiplayer_pressed() -> void:
 	multiplayer_pressed.emit()
+
+func _on_update_game():
+	JavaScriptBridge.eval("updateServiceWorker()")
