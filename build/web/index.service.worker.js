94b0725a9baa382b3ae9dcfa66b75f59217c05eb
Brian Ingles <github@emeraldwalk.com>
2025-06-18 18:11:11 -0500

    VRAM compression

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750253792|9535505';
+const CACHE_VERSION = '1750288262|7319893';
 /** @type {string} */
 const CACHE_PREFIX = 'stick-world-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
