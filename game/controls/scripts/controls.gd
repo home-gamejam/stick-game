c0e88dd51c48ff5eecb28b7b6ddd7fce23b2b3ea
Brian Ingles <github@emeraldwalk.com>
2025-06-17 22:12:37 -0500

    reload game button

--- game/controls/scripts/controls.gd
+++ game/controls/scripts/controls.gd
@@ -2,9 +2,13 @@ extends MarginContainer
 
 func _ready() -> void:
 	%LeftStick.stick_position_set.connect(_on_left_stick_position_set)
+	%Restart.pressed.connect(_on_restart)
 
 func _on_left_stick_position_set(position_: Vector2) -> void:
 	%Debug.text = str(position_)
 
-func _on_reset_button_pressed():
-	Signals.reset_game.emit()
+func _on_restart():
+	if OS.get_name() == "Web":
+		JavaScriptBridge.eval("reloadPage()")
+	else:
+		get_tree().change_scene_to_file("res://main/game.tscn")
