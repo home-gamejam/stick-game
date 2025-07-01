72d6765358d037b516bebf22b795f36dac25aace
Brian Ingles <github@emeraldwalk.com>
2025-06-30 23:26:09 -0500

    Latest web build

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751232051|7745245';
+const CACHE_VERSION = '1751343956|7953216';
 /** @type {string} */
 const CACHE_PREFIX = 'stick-world-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
